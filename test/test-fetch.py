adea4b8c1a61269821977775c0dadd0062bbc470
yessaimon <149254883+yessaimon@users.noreply.github.com>
2026-07-11 00:06:26 +0600

    Update weather site URL in test-fetch.py

--- test/test-fetch.py
+++ test/test-fetch.py
@@ -1,6 +1,6 @@
 import asyncio
 from async_scraper import fetch, close
-site = 'https://www.accuweather.com/ru/kg/bosteri/226688/weather-forecast/226688'
+site = 'https://example.com/weather'
 graduse = 'span'
 async def main():
     temperatura = await fetch(
